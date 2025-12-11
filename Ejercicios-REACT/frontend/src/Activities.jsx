95b4360d4b67d2f30d46a52caacdb00ec34e1119
Facu Arribilaga <2411006@ucc.edu.ar>
2025-12-11 16:42:44 -0300

    REACT 2: Redirección de Rutas (Parte 1)

---
+++ Ejercicios-REACT/frontend/src/Activities.jsx
@@ -0,0 +1,42 @@
+const Activities = () => {
+    const activities = [
+        {
+          nombre: "taekwondo",
+          descripcion: "Arte marcial coreana",
+          horarios: [
+            { dia: 2, "hora-inicio": "18:30", "hora-fin": "20:00" },
+            { dia: 4, "hora-inicio": "18:30", "hora-fin": "20:00" }
+          ]
+        },
+        {
+          nombre: "zumba",
+          descripcion: "ritmos latinos",
+          horarios: [
+            { dia: 1, "hora-inicio": "19:30", "hora-fin": "20:30" },
+            { dia: 3, "hora-inicio": "19:30", "hora-fin": "20:30" }
+          ]
+        }
+    ];
+
+    const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+
+    return (
+        <div className="activitiesContainer">
+            {activities.map((activity, index) => (
+                <div className="activityCard" key={index}>
+                    <h3>{activity.nombre}</h3>
+                    <p>{ac.descripcion}</p>
+                    <ul>
+                        {activities.horarios.map((horario, i) => (
+                            <li key={i}>
+                                Día: (horario.dia)
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Activities;
--- Ejercicios-REACT/frontend/src/Login.jsx
+++ Ejercicios-REACT/frontend/src/Login.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
 
         if(username == "admin" && password == "admin"){
             console.log("Login OK");
+            navigate("/actividades");
         }else{
             console.log("Login Incorrecto");
         }
