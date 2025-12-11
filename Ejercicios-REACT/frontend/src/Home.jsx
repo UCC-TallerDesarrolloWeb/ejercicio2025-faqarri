e853c1ca68bdaece355feed6b7efe0eac7866703
Facu Arribilaga <2411006@ucc.edu.ar>
2025-12-11 17:27:55 -0300

    REACT 3: Outlet

---
+++ Ejercicios-REACT/frontend/src/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+        <h2>Informacion del Gym</h2>
+    )
+}
+
+export default Home;
--- Ejercicios-REACT/frontend/src/main.jsx
+++ Ejercicios-REACT/frontend/src/main.jsx
@@ -3,14 +3,19 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css';
 import Login from './Login.jsx';
+import Layout from './Layout.jsx';
+import Home from './Home.jsx';
 import Activities from './activities.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/actividades' element={<Activities/>}/>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/actividades' element={<Activities/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
