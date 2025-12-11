4ee00cc4a3213775e27d56a191a9138b441880b4
Facu Arribilaga <2411006@ucc.edu.ar>
2025-12-11 18:08:31 -0300

    REACT: Imagenes

--- Ejercicios-REACT/frontend/src/pages/Home.jsx
+++ Ejercicios-REACT/frontend/src/pages/Home.jsx
@@ -1,6 +1,27 @@
+import yogaImg from "../assets/home/yoga.webp";
+import zumbaImg from "../assets/home/zumba.jpg";
+import "@styles/Home.css";
+
 const Home = () => {
     return(
-        <h2>Informacion del Gym</h2>
+        <div className="cardHome">
+            <div>
+                <img src="../../public/home/funcional.jpg" alt="Funcional" />
+                <h2>Funcional</h2>
+            </div>
+            <div>
+                <img src="../../public/home/taekwondo.jpg" alt="Taekwondo" />
+                <h2>Taekwondo</h2>
+            </div>
+            <div>
+                <img src={yogaImg} alt="Yoga" />
+                <h2>Yoga</h2>
+            </div>
+            <div>
+                <img src={zumbaImg} alt="Zumba" />
+                <h2>Zumba</h2>
+            </div>
+        </div>
     )
 }
 
